4f3c9ecd15522c38cfe6e58aa44a414974bb0402
Haardik <ravat.1@iitj.ac.in>
2022-03-01 22:46:06 +0530

    Mui edits

---
+++ src/pages/Popup/Play.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+export default function PlayBtn() {
+  return (
+      <IconButton >
+        <DeleteIcon fontSize="inherit" />
+      </IconButton>
+  );
+}
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,17 +3,23 @@ import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import SelectAutoWidth from '../Popup/List';
 import './Popup.css';
+import VSlider from '../Popup/Sound';
+import PlayBtn from '../Popup/Play';
 
 const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
         <SelectAutoWidth/>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
+        <PlayBtn/>
+        <VSlider/>
       </header>
+      
     </div>
   );
 };
