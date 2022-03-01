ccda89aa5c87f645bdad3fe36784e2241d76bfe7
Haardik <ravat.1@iitj.ac.in>
2022-03-01 22:04:07 +0530

    base changes

---
+++ src/pages/Popup/List.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function SelectAutoWidth() {
+  const [location, setAge]=React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 250 }}>
+        <InputLabel>Location</InputLabel>
+        <Select
+          value={location}
+          onChange={handleChange}
+          autoWidth
+          label="Location"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Twenty</MenuItem>
+          <MenuItem value={21}>Twenty one</MenuItem>
+          <MenuItem value={22}>Twenty one and a half</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
