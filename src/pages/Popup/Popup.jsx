9803038325dcac03ce253fdd77d7ee8835e55db6
Haardik <ravat.1@iitj.ac.in>
2022-03-01 22:04:15 +0530

    imports

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
+import SelectAutoWidth from '../Popup/List';
 import './Popup.css';
 
 const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <SelectAutoWidth/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
